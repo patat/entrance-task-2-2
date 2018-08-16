b6fda1a9ec01e4b9c7b7eab7d6cd4d6d8f8ddebd
Patat <nemnogosloven@gmail.com>
2018-08-16 17:18:16 +0300

    refactor and update main-menu

--- src/modules/main-menu/main-menu.js
+++ src/modules/main-menu/main-menu.js
@@ -1,11 +1,9 @@
 export default function mainMenu() {
   const container = document.querySelector('.main-menu__container');
-  const menu = document.querySelector('.main-menu')
   const burger = document.querySelector('.main-menu__burger');
 
   burger.addEventListener('click', () => {
-    container.classList.toggle('main-menu__container--active');
-    menu.classList.toggle('main-menu--active');
+    container.classList.toggle('main-menu__container--hidden');
     burger.classList.remove('main-menu__burger-icon--active');
     console.log('click');
   });
@@ -13,4 +11,17 @@ export default function mainMenu() {
   burger.addEventListener('pointerdown', () => {
     burger.classList.add('main-menu__burger-icon--active');
   });
+
+  /* 
+   * Menu Items
+   */
+   const menuItems = document.querySelectorAll('.main-menu__item');
+   let currActiveMenuItem = 0;
+   menuItems.forEach((menuItem, index) => {
+    menuItem.addEventListener('click', function(ev) {
+      menuItems[currActiveMenuItem].classList.remove('main-menu__item--active');
+      this.classList.add('main-menu__item--active');
+      currActiveMenuItem = index;
+    });
+   });
 }
